c006c8116cf9dc3ea849b854dadca3801bfe0c77
rabongHan <recognize0117@gmail.com>
2021-08-22 00:01:57 +0900

    eighth

--- routes/api.js
+++ routes/api.js
@@ -5,15 +5,12 @@ const school = new School()
 
 school.init(School.Type.HIGH, School.Region.GYEONGGI, 'J100005286')
 
-apiRouter.get('/keyboard', function(req, res) {
-   const data = {'type' : 'text'}
-   res.json(data);  
-});
+
 //문제점: 01 은 1로 바뀌어서 변환되어야함. 21은 괜찮 but 01,09는 안괜찮
  apiRouter.post('/monthlymeal', async function(req,res) {
-    const monthlydatetime = req.body.action.params.date;
+    const monthlydatetime = req.body.action.detailedParams.date.origin;
     const monthlydate = JSON.stringify(monthlydatetime);
-    const monthlymeal_result = monthlydate.substring(18,20);
+    const monthlymeal_result = monthlydate.substring(9,11);
 
     const meal2 = await school.getMeal({default: '이 날은 급식이 없습니다.'});
     const monthly_printing = meal[monthlymeal_result];
