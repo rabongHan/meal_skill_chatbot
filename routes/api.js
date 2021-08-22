73bf8a4979c6ca535b6e10c334288b5f896b2945
rabongHan <recognize0117@gmail.com>
2021-08-22 22:39:09 +0900

    0822 fifth

--- routes/api.js
+++ routes/api.js
@@ -5,34 +5,33 @@ const school = new School()
 
 school.init(School.Type.HIGH, School.Region.GYEONGGI, 'J100005286')
 
- apiRouter.get('/keyboard', (req, res) => {
-    const data = { 'type': 'text' }
-    res.json(data);
- });
+apiRouter.get('/keyboard', function(req, res) {
+  const data = {'type' : 'text'}
+  res.json(data);  
+});
 //문제점: 01 은 1로 바뀌어서 변환되어야함. 21은 괜찮 but 01,09는 안괜찮
- apiRouter.post('/monthlymeal', async function(req,res) {
-    const userAction = req.body.action;
-    const preMap = userAction.params;
-    const monthlydate = preMap.get('date')
-    const monthlymeal_result = monthlydate.substring(18,20);
+apiRouter.post('/monthlymeal', async function(req,res) {
+  const preMap = req.body.action.params;
+  const monthlydate = preMap.get("date");
+  const monthlymeal_result = monthlydate.substring(18,20);
 
-    const meal2 = await school.getMeal({default: '이 날은 급식이 없습니다.'});
-    const monthly_printing = meal[monthlymeal_result];
+  const meal2 = await school.getMeal({default: '이 날은 급식이 없습니다.'});
+  const monthly_printing = meal2[monthlymeal_result];
 
-    console.log(req.body);
+  console.log(req.body);
 
-    const responseBody = {
-     version: "2.0",
-     template: {
-       outputs: [
-         {
-           simpleText: {
-             text: `${meal2.month}월 ${monthlymeal_result}일 급식정보 \n` + monthly_printing
-           }
-         }
-       ] 
-     }
-   };
-   res.status(200).send(responseBody)
- });
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: `${meal2.month}월 ${monthlymeal_result}일 급식정보 \n` + monthly_printing
+          }
+        }
+      ] 
+    }
+  };
+  res.status(200).send(responseBody)
+});
 module.exports = apiRouter;
