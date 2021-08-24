06b7eda942e2a5f768a8a1c9a478e05f3f90046a
rabongHan <recognize0117@gmail.com>
2021-08-24 12:59:36 +0900

    0824 20th todaymeal added

--- routes/api.js
+++ routes/api.js
@@ -25,7 +25,7 @@ apiRouter.post('/monthlymeal', async function(req,res) {
   const monthlydate = JSON.stringify(preMap);
   // const monthlymeal_result = monthlydate.substring(30,32); for params 
   if(mobileChecking) {
-    var monthlymeal_result = monthlydate.substring(42,44);
+    var monthlymeal_result = monthlydate.substring(42,44); //40,42
   } else {
     var monthlymeal_result = monthlydate.substring(34,36);
   }
@@ -72,6 +72,27 @@ apiRouter.post('/tomorrowmeal', async function(req,res) {
         {
           simpleText: {
             text: `${meal3.month}월 ${tomorrow_string}일 급식정보 \n` + tomorrow_printing
+          }
+        }
+      ] 
+    }
+  };
+  res.json(responseBody)
+});
+
+apiRouter.post('/todaymeal', async function(req,res) {
+  const meal4 = await school.getMeal({default: '이 날은 급식이 없습니다.'});
+  const today_printing = meal4[meal4.day];
+
+  console.log(req.body);
+
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: `${meal4.month}월 ${meal4.day}일 급식정보 \n` + today_printing
           }
         }
       ] 
