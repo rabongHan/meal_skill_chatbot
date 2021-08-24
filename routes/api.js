06054dea0f429baed571ab5bd037e15824069a5e
rabongHan <recognize0117@gmail.com>
2021-08-24 12:39:26 +0900

    0824 19th testversion

--- routes/api.js
+++ routes/api.js
@@ -25,7 +25,7 @@ apiRouter.post('/monthlymeal', async function(req,res) {
   const monthlydate = JSON.stringify(preMap);
   // const monthlymeal_result = monthlydate.substring(30,32); for params 
   if(mobileChecking) {
-    var monthlymeal_result = monthlydate.substring(40,42);
+    var monthlymeal_result = monthlydate.substring(42,44);
   } else {
     var monthlymeal_result = monthlydate.substring(34,36);
   }
