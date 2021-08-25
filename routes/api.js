0df0e2a40c4ff05e6faa0fea97075f7654398f3b
rabongHan <recognize0117@gmail.com>
2021-08-25 23:43:49 +0900

    0825 17th

--- routes/api.js
+++ routes/api.js
@@ -233,7 +233,7 @@ apiRouter.post('/studenttimetable', async function(req,res) {
       outputs: [
         {
           simpleText: {
-            text: cut_student_num 
+            text: timetable_printing 
           }
         }
       ] 
