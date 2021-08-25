aa6e4ac1d0c9fa71f894f92341c5e27953f5e799
rabongHan <recognize0117@gmail.com>
2021-08-25 23:48:05 +0900

    0825 18th

--- routes/api.js
+++ routes/api.js
@@ -224,7 +224,7 @@ apiRouter.post('/studenttimetable', async function(req,res) {
   const temp_student_num = userDB[userId];
   const cut_student_num = temp_student_num.substring(0,2) + temp_student_num.substring(3,4);
   const timetable_printing = JSON.stringify(student_timetable[cut_student_num]);
-
+  const checking_example = cut_student_num == Object.keys(student_timetable)[0]
   console.log(req.body);
 
   const responseBody = {
@@ -233,7 +233,7 @@ apiRouter.post('/studenttimetable', async function(req,res) {
       outputs: [
         {
           simpleText: {
-            text: timetable_printing 
+            text: checking_example 
           }
         }
       ] 
