a09d513f226129cb4b5ea6a3d71510ffeeb11008
rabongHan <recognize0117@gmail.com>
2021-08-25 23:37:11 +0900

    0825 13th

--- routes/api.js
+++ routes/api.js
@@ -214,14 +214,13 @@ apiRouter.post('/changeStudentNum', async function(req,res) {
   };
   res.json(responseBody)
 });
-var student_timetable = {
-  "33" : {"1교시" : "국어", "2교시": "수학"}
-};
 
 // 시간표 챗봇 코드 
 apiRouter.post('/studenttimetable', async function(req,res) {
   const userId = req.body.userRequest.user.id; //kakao 식별자
-
+  const student_timetable = {
+    "33" : {"1교시" : "국어", "2교시": "수학"}
+  };
   const temp_student_num = userDB[userId];
   const cut_student_num = temp_student_num.substring(0,1) + temp_student_num.substring(2,3);
   const timetable_printing = student_timetable[cut_student_num];
@@ -234,7 +233,7 @@ apiRouter.post('/studenttimetable', async function(req,res) {
       outputs: [
         {
           simpleText: {
-            text: cut_student_num + `${typeof(cut_student_num)} ${typeof(Object.keys(student_timetable)[0])}`
+            text: cut_student_num + `${typeof(cut_student_num)}`
           }
         }
       ] 
