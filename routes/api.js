8dd5bed038446f4d4d420663696ee8159a100c14
rabongHan <recognize0117@gmail.com>
2021-09-15 08:19:42 +0900

    0915 1st

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const apiRouter = require("express").Router();
 const modernizr = require("modernizr"); //mobile checking
 // for db 
 const {Prohairesis} = require("prohairesis"); 
-const env = require("./env");
+const env = require("../env");
 const database = new Prohairesis(env.CLEARDB_DATABASE_URL);
 // 
 
