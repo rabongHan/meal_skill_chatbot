cf664f786a6fcf4d4e5a3a23b78b2679e2ffb7a6
rabongHan <recognize0117@gmail.com>
2021-09-17 12:27:03 +0900

    sadfadsafsdf

--- routes/api.js
+++ routes/api.js
@@ -152,7 +152,7 @@ apiRouter.post('/addStudentNum', async function(req,res) {
   var finalText = "";
   var preCheckingString = JSON.stringify(await getIfThere());
   var checkingStudentExist = preCheckingString.substring(12,13);
-  if(checkingStudentExist == "1") {
+  if(checkingStudentExist == "0") {
     const InsertingQuery = 'INSERT INTO Board (username, studentid)  VALUES (?,?)';
     connection_sql.query(InsertingQuery, [userId, userStudentNum], function(err,results) {
       if(err) throw err;
