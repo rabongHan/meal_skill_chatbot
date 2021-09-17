422dc0b6406fe88e94146171ba46e320b3310ecd
rabongHan <recognize0117@gmail.com>
2021-09-17 12:36:37 +0900

    sdfsd

--- routes/api.js
+++ routes/api.js
@@ -168,7 +168,7 @@ apiRouter.post('/addStudentNum', async function(req,res) {
     template: {
       outputs: [
         {
-          basicCard: { //p
+          basicCard: { //ps
             description: `${finalText} // ${checkingStudentExist}`
           }
         }
