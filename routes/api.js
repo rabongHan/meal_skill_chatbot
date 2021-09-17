5aaa05c362385b9098f328e04a938209c52760ec
rabongHan <recognize0117@gmail.com>
2021-09-17 17:46:19 +0900

    sdfasd

--- routes/api.js
+++ routes/api.js
@@ -134,7 +134,7 @@ apiRouter.post('/addStudentNum', async function(req,res) {
   };
   
   async function getIfThere() {
-    var output = await getInfo("SELECT EXISTS(SELECT 1 FROM board WHERE username='${userId}');");
+    var output = await getInfo("SELECT EXISTS(SELECT 1 FROM board WHERE username='${userId}') as SUCCESS;");
     return output;
   }
   
@@ -152,7 +152,7 @@ apiRouter.post('/addStudentNum', async function(req,res) {
   
   var finalText = "";
   var preCheckingString = JSON.stringify(await getIfThere());
-  var checkingStudentExist = preCheckingString.substring(50,60);
+  var checkingStudentExist = preCheckingString.substring(12,13);
   if(checkingStudentExist == "0") {
     const InsertingQuery = 'INSERT INTO Board (username, studentid)  VALUES (?,?)';
     connection_sql.query(InsertingQuery, [userId, userStudentNum], function(err,results) {
