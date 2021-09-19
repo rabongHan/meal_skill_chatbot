e37aac7d35e93d0157191dbd757b63486182d6eb
rabongHan <recognize0117@gmail.com>
2021-09-19 18:43:23 +0900

    0919 3

--- routes/api.js
+++ routes/api.js
@@ -134,7 +134,7 @@ apiRouter.post('/addStudentNum', async function(req,res) {
   };
   
   async function getIfThere() {
-    var output = await getInfo("SELECT EXISTS(SELECT 1 FROM board WHERE username='${userId}' as SUCCESS)");
+    var output = await getInfo("SELECT EXISTS(SELECT 1 FROM board WHERE username='${userId}') as SUCCESS");
     return output;
   }
   
